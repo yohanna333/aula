2a8850b22f05ce117dd8a90f69f9d8d943aa27fb
yohanna333 <yohanna.hegele@escola.pr.gov.br>
2026-05-27 11:36:54 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+const body = document.body;
+body.style.margin = '0'
+
+const cabecalho = document.createElement('h1')
+cabecalho.textContent = 'criando elementos com JavaScript';
+
+cabecalho.style.color = '#ffffff';
+cabecalho.style.fontSize = '40px';
+cabecalho.style.textAlign = 'center';
+cabecalho.style.display = 'flex';
+cabecalho.style.alignItems = 'center';
+cabecalho.style.justifyContent = 'center';
+cabecalho.style.backgroundcolor ='#3a'
+cabecalho.style.
+cabecalho.style.
+cabecalho.style.
+cabecalho.style.
+cabecalho.style.
+cabecalho.style.
+cabecalho.style.
+cabecalho.style.
